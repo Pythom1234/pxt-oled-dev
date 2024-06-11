cad02ff50a7faf02529facedb175df5fb1d42848
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-06-11 17:55:39 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -131,7 +131,6 @@ namespace OLED {
     //% toggle.defl=false
     //% weight=94
     export function drawText(text: string, x: number, y: number, color: boolean, toggle: boolean): void {
-        serial.writeLine("sdafdsfsdf")
         const font = [
             "2,0 3,0 1,1 4,1 0,2 5,2 0,3 5,3 0,4 1,4 2,4 3,4 4,4 5,4 0,5 5,5 0,6 5,6 0,7 5,7",
             "0,0 1,0 2,0 3,0 4,0 0,1 5,1 0,2 5,2 0,3 1,3 2,3 3,3 4,3 0,4 5,4 0,5 5,5 0,6 5,6 0,7 1,7 2,7 3,7 4,7",
