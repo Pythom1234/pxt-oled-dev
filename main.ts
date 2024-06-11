e950e76f3d6399d655bd5c3f2c09811f8c6355c3
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-06-11 17:36:00 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -210,7 +210,7 @@ namespace OLED {
 
         const fontIndex = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', ',', '?', '.', '!', '<', '>', '/', ':', '*', '-', '+', '=']
 
-        function getChar(char: string): Array<Array<number>> {
+        function getChar(char: string): number[][] {
             let out: number[][] = []
             if (charsetIndex.indexOf(char) != -1) {
                 console.log(charset)
